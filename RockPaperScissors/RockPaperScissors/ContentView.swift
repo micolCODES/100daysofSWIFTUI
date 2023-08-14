efd067ebd1582bc11aa24374ab8ddc0ce8038955
micolCODES <micolromano124@gmail.com>
2023-08-14 20:54:00 +0200

    add Rock Paper Scissors

---
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Micol on 14.08.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Rock Paper Scissors")
+            Text("Here we go!!!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
