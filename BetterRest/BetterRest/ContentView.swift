59635b663d3be60616c1e91dc283c2b73cd3f754
micolCODES <micolromano124@gmail.com>
2023-08-18 11:38:39 +0200

    explore DatePicker

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -9,12 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
     
     var body: some View {
         VStack {
             Stepper("\(sleepAmount.formatted()) hour", value: $sleepAmount, in: 4...12, step: 0.25)
+            DatePicker("DatePicker 1", selection: $wakeUp, displayedComponents: .hourAndMinute)
+            DatePicker("DatePicker 2", selection: $wakeUp, in: Date.now...)
+                //.labelsHidden()
         }
         .padding()
+    }
+    func exampleDate() {
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        let range = Date.now...tomorrow
     }
 }
 
