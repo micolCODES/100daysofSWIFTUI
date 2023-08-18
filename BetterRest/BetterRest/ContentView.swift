a9a2adcef4d4a14d46557ec1b02f2eb9ded60e5e
micolCODES <micolromano124@gmail.com>
2023-08-18 19:37:56 +0200

    add Calculate button and function to calculate

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -4,13 +4,17 @@
 //
 //  Created by Micol on 18.08.23.
 //
-
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
@@ -26,9 +30,41 @@ struct ContentView: View {
             }
             .navigationTitle("BetterRest")
             .toolbar{
-                //Button("Calculate")
+                Button("Calculate", action: calculateBedtime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert){
+                Button("OK") {}
+            } message: {
+                Text(alertMessage)
+            }
         }
+        
     }
+    
+    func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            //more code goes here
+            alertTitle = "You ideal bedtime is "
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            //Something went wrong
+            alertTitle = "Error"
+            alertMessage = "There was a problem"
+            
+        }
+        showingAlert = true
+    }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
