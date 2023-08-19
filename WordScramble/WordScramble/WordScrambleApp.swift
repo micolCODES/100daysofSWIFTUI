91a21cf5c3d59284fd99410c71acba086536a709
micolCODES <micolromano124@gmail.com>
2023-08-19 20:27:27 +0200

    initial commit

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Micol on 19.08.23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
