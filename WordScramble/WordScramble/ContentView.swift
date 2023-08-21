efac02e70a173fc28566b4fe8a0ddd9464250c6e
micolCODES <micolromano124@gmail.com>
2023-08-21 20:09:15 +0200

    reset file

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,33 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["John", "Jack", "Jill"]
     var body: some View {
-        List(people, id: \.self){
-            Text($0)
-        }
-//        List(0..<5){
-//            Text("Dynamic Item #\($0)")
-//        }
-//        List {
-//            Section("Section 1"){
-//                Text("Item #1")
-//                ForEach(0..<5) {
-//                    Text("Dynamic Item \($0)")
-//                }
-//            }
-//            Section("Section 2") {
-//                Text("Item #2")
-//                Text("Item #3")
-//            }
-//        }
-//        VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundColor(.accentColor)
-//            Text("Hello, world!")
-//        }
-//        .padding()
+        Text("Hello, world!")
+            .padding()
     }
 }
 
