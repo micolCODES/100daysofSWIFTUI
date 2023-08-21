33c20d0132955ffc40462d4f74c04c43ac2efe0f
micolCODES <micolromano124@gmail.com>
2023-08-21 20:39:07 +0200

    restart project

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -12,14 +12,6 @@ struct ContentView: View {
         Text("Hello, world!")
             .padding()
     }
-    
-    func test() {
-        let word = "swift"
-        let checker = UITextChecker()
-    
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
