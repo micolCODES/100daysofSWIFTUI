7fc0622f963bedc61ff4ed7cf6b10097e269e2b8
micolCODES <micolromano124@gmail.com>
2023-08-25 14:13:32 +0200

    explore custom transitions

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,23 +7,46 @@
 
 import SwiftUI
 
+struct cornerRotateModifier : ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(active: cornerRotateModifier(amount: -90, anchor: .topLeading), identity: cornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+}
+
 struct ContentView: View {
     @State private var isShowingRed = false
-    //@State private var dragAmount = CGSize.zero
+    
     
     var body: some View {
-        VStack {
-            Button("Tap me") {
-                withAnimation{                isShowingRed.toggle()
-                }
-                }
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
             
             if isShowingRed {
                 Rectangle()
                     .fill(.red)
                     .frame(width: 200, height: 200)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .transition(.pivot)
+                
             }
+        }
+        .onTapGesture {
+            withAnimation{
+                isShowingRed.toggle()
+            }
+            
         }
     }
 }
