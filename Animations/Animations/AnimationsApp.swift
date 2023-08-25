00db2024bd57259556c724fdd24d23eb7e37d6ac
micolCODES <micolromano124@gmail.com>
2023-08-25 11:41:25 +0200

    initial commit

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Micol on 25.08.23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
