cd0a44191d1d355d26e1918d6b5d78dd28b15c79
micolCODES <micolromano124@gmail.com>
2023-08-25 12:15:31 +0200

    explore animating bindings

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -11,31 +11,25 @@ struct ContentView: View {
     @State private var animationAmuont = 1.0
     
     var body: some View {
-        Button("Tap me"){
-            //jumpy animation, in clear steps
-            //must always be here, otherwise animation will activate on ANY change, even change in phone orientation
-            animationAmuont += 1
-        }
+        print(animationAmuont)
+        
+        return VStack {
+            Stepper("Scale amount", value: $animationAmuont.animation(
+                .easeInOut(duration: 1)
+                .repeatCount(2, autoreverses: true)
+            ), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap me") {
+                animationAmuont += 1
+            }
             .padding(50)
             .background(.red)
             .foregroundColor(.white)
             .clipShape(Circle())
-            .overlay(
-                Circle()
-                    .stroke(.red)
-                    .scaleEffect(animationAmuont)
-                    .opacity(2 - animationAmuont)
-                    .animation(
-                        .easeInOut(duration: 13)
-                        .repeatCount(3, autoreverses: false),
-                        value: animationAmuont)
-            )
-            .onAppear{
-                animationAmuont = 2
-            }
-            //.scaleEffect(animationAmuont)
-            //.blur(radius: (animationAmuont - 1 ) * 3)
-        //smooth animation
+            .scaleEffect(animationAmuont)
+        }
             
     }
 }
