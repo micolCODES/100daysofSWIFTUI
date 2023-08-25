1a36f2824dce0871d53d641599963b587e201765
micolCODES <micolromano124@gmail.com>
2023-08-25 12:07:53 +0200

    explore custom animation and overlays

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,19 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmuont = 1.0
+    
     var body: some View {
         Button("Tap me"){
             //jumpy animation, in clear steps
+            //must always be here, otherwise animation will activate on ANY change, even change in phone orientation
             animationAmuont += 1
         }
             .padding(50)
             .background(.red)
             .foregroundColor(.white)
             .clipShape(Circle())
-            .scaleEffect(animationAmuont)
-            .blur(radius: (animationAmuont - 1 ) * 3)
+            .overlay(
+                Circle()
+                    .stroke(.red)
+                    .scaleEffect(animationAmuont)
+                    .opacity(2 - animationAmuont)
+                    .animation(
+                        .easeInOut(duration: 13)
+                        .repeatCount(3, autoreverses: false),
+                        value: animationAmuont)
+            )
+            .onAppear{
+                animationAmuont = 2
+            }
+            //.scaleEffect(animationAmuont)
+            //.blur(radius: (animationAmuont - 1 ) * 3)
         //smooth animation
-            .animation(.default, value: animationAmuont)
+            
     }
 }
 
