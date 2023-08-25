21e9df8643d62e98928b1060a0f648897866ff1f
micolCODES <micolromano124@gmail.com>
2023-08-25 13:16:52 +0200

    exploring animation stacking

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,19 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmuont = 0.0
+    @State private var enabled = false
     
     var body: some View {
         Button("Tap me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                animationAmuont += -360
-            }
+            enabled.toggle()
         }
-        .padding(50)
-        .background(.red)
+        .frame(width: 200, height: 200)
+        .background(enabled ? .blue : .gray)
+        .animation(.default, value: enabled)
         .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmuont), axis: (x:0, y:0, z:1))
+        .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+        .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
     }
 }
 
