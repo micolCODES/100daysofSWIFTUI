941af9948e579f4c6f1e7a3144ec0acedf698d4c
micolCODES <micolromano124@gmail.com>
2023-08-27 20:19:30 +0200

    initial commit

---
+++ MultiplicationTables/MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Micol on 27.08.23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
