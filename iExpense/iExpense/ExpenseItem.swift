a162fd461c1336a97661350ac604ed4d9a2b1064
micolCODES <micolromano124@gmail.com>
2023-09-04 17:00:31 +0200

    add UserDefault to make added items permanently available

--- iExpense/iExpense/ExpenseItem.swift
+++ iExpense/iExpense/ExpenseItem.swift
@@ -8,9 +8,11 @@
 import Foundation
 
 // Identifyable means it has a unique identifier, in this case, the self-generated UUID. If I use this, I don't need to tell the ForEach loop how to identify the items, since I've marked them as identifiable
+//Codable means the class Expenses can encode and save with JSONencoder and UserDefaults
 
-struct ExpenseItem : Identifiable {
-    let id = UUID()
+struct ExpenseItem : Identifiable, Codable {
+    //now I get alert that id is immutable (if it's set with "LET"). This is ok, but you can change it to "VAR" to  make the alert go away.
+    var /*let*/ id = UUID()
     let name: String
     let type: String
     let amount: Double
--- iExpense/iExpense/Expenses.swift
+++ iExpense/iExpense/Expenses.swift
@@ -8,5 +8,29 @@
 import Foundation
 
 class Expenses : ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            
+            if let encoded = try? /* NOTE 1 for error here*/encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    //Code will save the data but it will not recall it/load it when the app is reloaded -> you need to add an initializer function here
+    init() {
+        //If you can read Items in UserDefaults then...
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            //... then please decoded them here, and IF you can decode them, then ...
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self/*this is what I EXPECT to receive. I use .self to explain that I am referring to the array of ExpenseItems by itelf, not a specific array, but that idea of an array*/, from: savedItems) {
+                // ... then store them into items
+                items = decodedItems
+                return
+            }
+        }
+        // if not, then item is empty
+        items = []
+    }
 }
+
+//NOTE 1: you get an error here if you forget to go to the class and add the "Codable" protocol. So basically you'll always get it as you code, and once the class is done, for to struct calling/defining the class and add the "Codable" protocol
