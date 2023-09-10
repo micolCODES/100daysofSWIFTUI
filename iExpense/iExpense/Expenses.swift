7465444da0aaeff0d608fe868c3509372593b3c9
micolCODES <micolromano124@gmail.com>
2023-09-10 16:59:25 +0200

    update to comments to make them even more usable and understandable

--- iExpense/iExpense/Expenses.swift
+++ iExpense/iExpense/Expenses.swift
@@ -19,12 +19,9 @@ class Expenses : ObservableObject {
     }
     //Code will save the data but it will not recall it/load it when the app is reloaded -> you need to add an initializer function here
     init() {
-        //If you can read Items in UserDefaults then...
-        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
-            //... then please decoded them here, and IF you can decode them, then ...
-            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self/*this is what I EXPECT to receive. I use .self to explain that I am referring to the array of ExpenseItems by itelf: not a specific array, but that idea of an array*/, from: savedItems) {
-                // ... then store them into items
-                items = decodedItems
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") { /*If you can read Items in UserDefaults then...*/
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self/*NOTE 2*/, from: savedItems) { /*... then please decoded them here...*/ //... and IF you can decode them, ...
+                items = decodedItems /*... then store them into items*/
                 return
             }
         }
@@ -33,4 +30,6 @@ class Expenses : ObservableObject {
     }
 }
 
-//NOTE 1: you get an error here if you forget to go to the class and add the "Codable" protocol. So basically you'll always get it as you code, and once the class is done, for to struct calling/defining the class and add the "Codable" protocol
+//NOTE 1: you get an error here if you forget to go to the struct and add the "Codable" protocol. "encoder.encode" only works with struct conforming to the Codable protocol. So basically you'll always get it as you code, and once the class is done, for to struct calling/defining the class and add the "Codable" protocol. The ERROR YOU GET IS -> X) Class 'JSONEncoder' requires that 'ExpenseItem' conform to 'Encodable'
+
+//NOTE 2: this is what I EXPECT to receive from the decoder (basically this is the blueprint of the item that I expect the decoder to make as it decodes, so like telling a ribosome that I expect a protein made up of a chain of single aminoacids from the mRNA copy it is "decoding"). I use .self to explain that I am referring to the array of ExpenseItems by itelf: not a specific array, but that type of array
