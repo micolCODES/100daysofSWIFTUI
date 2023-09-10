fbd4890815c96f5774324b4cdecacdc044be0c67
micolCODES <micolromano124@gmail.com>
2023-09-10 13:23:19 +0200

    new checkpoint

--- iExpenseREDO/iExpenseREDO/AddView.swift
+++ iExpenseREDO/iExpenseREDO/AddView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct AddView: View {
+    @ObservedObject var expenses = Expenses()
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
 
+    var types = ["Personal", "Business"]
+    
     var body: some View {
-       Text("Hello")
+        NavigationView {
+            Form {
+                TextField("Name of expense", text: $name)
+                Picker("Type of expense", selection: $type) {
+                    ForEach(types, id: \.self){
+                        Text($0)
+                    }
+                }
+                TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+            }
+            .toolbar {
+                Button {
+                    var newItem = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(newItem)
+                    dismiss()
+                } label: {
+                    Text("Add Item")
+                }
+            }
+        }
     }
     
 }
--- iExpenseREDO/iExpenseREDO/ContentView.swift
+++ iExpenseREDO/iExpenseREDO/ContentView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    @State private var showAddView = false
     
     var body: some View {
-        Text("Hello")
+        NavigationView {
+            List {
+                ForEach(expenses.items){ item in
+                    HStack {
+                        VStack(alignment: .leading){
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        VStack(alignment: .trailing){
+                            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                        }
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpenseREDO")
+            .toolbar {
+                Button {
+                    showAddView = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showAddView){
+                AddView(expenses: expenses)
+            }
+        }
     }
 // ---------- METHODS GO HERE ----------
-    
+    func removeItems(at offsets: IndexSet){
+        expenses.items.remove(atOffsets: offsets)
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
