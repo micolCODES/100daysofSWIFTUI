ad8ef9f835d428909155342ea4ebf7ca9dba510c
micolCODES <micolromano124@gmail.com>
2023-09-11 20:37:46 +0200

    initial commit

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Micol on 11.09.23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
