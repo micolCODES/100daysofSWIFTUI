30e57759c7fa07901b91364dd4dc943e56de56c2
micolCODES <micolromano124@gmail.com>
2023-09-23 18:23:08 +0200

    initial commit

---
+++ Drawing/Drawing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Micol on 23.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
