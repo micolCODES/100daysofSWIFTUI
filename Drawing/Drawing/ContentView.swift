cc6a9a0d8927cef2c04bbfaf9f6d90563b0234cf
micolCODES <micolromano124@gmail.com>
2023-09-28 15:21:29 +0200

    add five point star

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,59 +7,135 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-    // How much to move this petal away from the center
-    var petalOffset: Double = -20
+struct Polygon : Shape {
+   var sides = 5
 
-    // How wide to make each petal
-    var petalWidth: Double = 100
+   func path(in rect : CGRect ) -> Path{
+        let center = CGPoint(x: rect.width / 2, y: rect.height / 2)
+        let radius = rect.width / 2
+       let smallRadius = radius * 0.6
+       
+//        let arrowCapWidth = 0.3
+//        let maxLength = rect.maxX * 0.9
 
-    func path(in rect: CGRect) -> Path {
-        // The path that will hold all petals
+       //get angle in radians, then double it because we want to skip the first vertex and go to the next
+        let angle = Double.pi * 2 / Double(sides) * 2
         var path = Path()
-
-        // Count from 0 up to pi * 2, moving up pi / 8 each time
-        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
-            // rotate the petal by the current value of our loop
-            let rotation = CGAffineTransform(rotationAngle: number)
-
-            // move the petal to be at the center of our view
-            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-
-            // create a path for this petal using our properties plus a fixed Y and height
-            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
-
-            // apply our rotation/position transformation to the petal
-            let rotatedPetal = originalPetal.applying(position)
-
-            // add it to our main path
-            path.addPath(rotatedPetal)
+        var startPoint = CGPoint(x: 0, y: 0)
+        
+        for side in 0 ..< (sides + 1) {
+           
+            let x = center.x + CGFloat(cos(Double(side) * angle)) * CGFloat (radius)
+            let y = center.y + CGFloat(sin(Double(side) * angle)) * CGFloat(radius)
+                  
+            let vertexPoint = CGPoint( x: x, y: y)
+            
+//            let arrowCapX = maxLength * 0.95
+//            let topArrowCapY = vertexPoint.y * (1 - arrowCapWidth)
+//            let bottomArrowCapY = vertexPoint.y * (1 + arrowCapWidth)
+            
+            if (side == 0) {
+                startPoint = vertexPoint
+                path.move(to: startPoint )
+            }
+            else {
+                path.addLine(to: vertexPoint)
+            }
         }
-
-        // now send the main path back
+       
+       for side in 0 ..< (sides + 1) {
+          
+           let x = center.x + CGFloat(cos(Double(side) * angle)) * CGFloat (smallRadius)
+           let y = center.y + CGFloat(sin(Double(side) * angle)) * CGFloat(smallRadius)
+                 
+           let vertexPoint = CGPoint( x: x, y: y)
+           
+//            let arrowCapX = maxLength * 0.95
+//            let topArrowCapY = vertexPoint.y * (1 - arrowCapWidth)
+//            let bottomArrowCapY = vertexPoint.y * (1 + arrowCapWidth)
+           
+           if (side == 0) {
+               startPoint = vertexPoint
+               path.move(to: startPoint )
+           }
+           else {
+               path.addLine(to: vertexPoint)
+           }
+       }
+       
+       
+        
         return path
+  }
+}
+
+struct Arrow: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        var shape = Path()
+        let arrowCapWidth = 0.3
+        let maxLength = rect.maxX * 0.9
+        let h = (((2 * maxLength) + 1) / 4).squareRoot()
+        
+        //WOOD (x ; y)
+        let woodX = rect.minX
+        let woodY = rect.minY + h
+        
+        //WOOD/EARTH (x ; y)
+        let earthX = maxLength
+        let earthY = rect.minY + h
+        
+//        //FIRE (x ; y)
+//        let fireX = maxLength / 2
+//        let fireY = rect.minY
+//        
+//        //WATER
+//        let waterX = (maxLength - 1) / 4
+//        let waterY = h + maxLength.squareRoot()
+//        
+//        
+//        //METAL
+//        let metalX = maxLength - waterX
+//        let metalY = h + maxLength.squareRoot()
+//        
+        //arrowcap
+        let arrowCapX = maxLength * 0.95
+        let topArrowCapY = woodY * (1 - arrowCapWidth)
+        let bottomArrowCapY = woodY * (1 + arrowCapWidth)
+        
+        //WOOD -> EARTH
+        //line
+        shape.move(to: CGPoint(x: woodX, y: woodY))
+        shape.addLine(to: CGPoint(x: earthX, y: earthY))
+        
+//        arrow tip
+        shape.move(to: CGPoint(x: earthX, y: earthY))
+        shape.addLine(to: CGPoint(x: arrowCapX, y: topArrowCapY))
+        shape.addLine(to: CGPoint(x: arrowCapX, y: bottomArrowCapY))
+        shape.closeSubpath()
+        
+        
+        
+    
+        return shape
     }
 }
 
-struct ContentView: View {
-    @State private var petalOffset = -20.0
-    @State private var petalWidth = 100.0
 
+struct ContentView: View {
+    
     var body: some View {
         
         VStack {
             
-            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-                .fill(.red, style: FillStyle(eoFill: true))
-
-            Text("Offset")
-            Slider(value: $petalOffset, in: -40...40)
-                .padding([.horizontal, .bottom])
-
-            Text("Width")
-            Slider(value: $petalWidth, in: 0...100)
-                .padding(.horizontal)
+            Text("Drawing practice")
+            Arrow()
+                .stroke(.black, style: StrokeStyle(lineWidth: 3, lineJoin: .miter))
+            Polygon()
+                .stroke(.black, style: StrokeStyle(lineWidth: 3, lineJoin: .miter))
+                .rotationEffect(Angle(degrees: -17))
         }
+        .padding()
     }
 }
 
