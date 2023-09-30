47c919776d24b75fafd4418738f9801279724eac
micolCODES <micolromano124@gmail.com>
2023-09-30 18:42:01 +0200

    add HabitInfoView

---
+++ HabitTracking/HabitTracking/HabitInfoView.swift
@@ -0,0 +1,42 @@
+//
+//  HabitInfoView.swift
+//  HabitTracking
+//
+//  Created by Micol on 30.09.23.
+//
+
+import SwiftUI
+
+struct HabitInfoView: View {
+    @StateObject var habits = Habits()
+    
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var description = ""
+    
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                //name
+                TextField("Name of Habit", text: $name)
+                //description
+                TextField("Description", text: $description, axis: .vertical)
+            }
+            .toolbar {
+                Button {
+                    let newHabit = Habit(name: name, description: description)
+                    habits.habits.append(newHabit)
+                    dismiss()
+                } label: {
+                    Text("Add Habit")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HabitInfoView()
+}
