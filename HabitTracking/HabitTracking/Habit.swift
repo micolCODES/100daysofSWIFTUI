97d245d06664149bafdfc03fa7e98b26caceffd3
micolCODES <micolromano124@gmail.com>
2023-10-04 10:40:39 +0200

    add Equatable to struct to do math on items

--- HabitTracking/HabitTracking/Habit.swift
+++ HabitTracking/HabitTracking/Habit.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Habit: Codable, Identifiable {
+struct Habit: Codable, Identifiable, Equatable {
     var id = UUID()
     var name: String
     var description: String
