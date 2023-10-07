d38714df9cd85a0973e3fe09f8ea00ef3a0f54dc
micolCODES <micolromano124@gmail.com>
2023-10-07 15:32:52 +0200

    exploring validating and disabling forms

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,49 +7,29 @@
 
 import SwiftUI
 
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
+    @State private var username = ""
+    @State private var email = ""
     
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
-            .task {
-                await loadData() //here is the AWAIT that we need since the function is ASYNC
+            Section {
+                Button("Create Account") {
+                    print("Creating account...")
+                }
             }
+            .disabled(disableForm)
+            //.disabled(username.isEmpty || email.isEmpty)
         }
     }
-    //this method might want to go to sleep in order to complete its work. !!!Cannot use the .onAppear!!! -> NEEDS "AWAIT"
-    func loadData() async {
-        //step 1 create URL
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("invalid URL")
-            return
-        }
-        //step 2: fetch data from URL
-        do{
-            //return into data is a tuple, data + metadata, so we use an underscore"_" to say "throw out the metadata"
-            let (data, _) = try await URLSession.shared.data(from: url)
-            //step 3: load data in response struct
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("invalid data")
-        }    }
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
+    }
 }
 
 #Preview {
