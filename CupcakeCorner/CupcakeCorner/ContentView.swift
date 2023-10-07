b3f48b1f91c1c230cba2fa42211df037e86c0062
micolCODES <micolromano124@gmail.com>
2023-10-07 16:03:51 +0200

    start over

--- CupcakeCorner/CupcakeCorner/ContentView.swift
+++ CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -8,28 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var username = ""
-    @State private var email = ""
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Username", text: $username)
-                TextField("Email", text: $email)
-            }
-            Section {
-                Button("Create Account") {
-                    print("Creating account...")
-                }
-            }
-            .disabled(disableForm)
-            //.disabled(username.isEmpty || email.isEmpty)
-        }
+        Text("🧁")
     }
     
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
-    }
 }
 
 #Preview {
