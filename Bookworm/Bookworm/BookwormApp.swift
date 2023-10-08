f1ed3366e3f2c0c8244e941c4ee2c33779bd149f
micolCODES <micolromano124@gmail.com>
2023-10-08 18:28:36 +0200

    initial commit

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Micol on 08.10.23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
