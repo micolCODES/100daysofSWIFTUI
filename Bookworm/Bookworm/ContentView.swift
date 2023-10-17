240ed0a49ab0e7728d8bccf7ac5f60153dddd794
micolCODES <micolromano124@gmail.com>
2023-10-17 22:54:07 +0200

    remove test data

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -8,28 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
-    
     var body: some View {
         VStack {
-            List(students) { student in
-                Text(student.name ?? "Unknown")
-            }
-            
-            Button("Add") {
-                let firstName = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-                let lastName = ["Granger", "Lovegood", "Potter", "Weasley"]
-                let chosenFirstName = firstName.randomElement()!
-                let chosenLastName = lastName.randomElement()!
-                
-                let student = Student(context: moc)
-                student.id = UUID()
-                student.name = "\(chosenFirstName) \(chosenLastName)"
-                
-                try? moc.save()
-            }
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
 
