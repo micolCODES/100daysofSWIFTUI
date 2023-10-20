4362a7eebdd814326556beedaaba0cc861c289b4
micolCODES <micolromano124@gmail.com>
2023-10-20 16:14:49 +0200

    add RatingView with custom star rating, add RatingView to AddBookView

---
+++ Bookworm/Bookworm/RatingView.swift
@@ -0,0 +1,50 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Micol on 20.10.23.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int
+    
+    var label = ""
+    var maximumRating = 5
+    
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    var body: some View {
+        HStack {
+            if label.isEmpty == false {
+                Text(label)
+            }
+            
+            ForEach(1..<maximumRating + 1, id: \.self) { number in
+                image(for: number)
+                    .foregroundColor(number > rating ? offColor : onColor)
+                    .onTapGesture {
+                        rating = number
+                    }
+            }
+        }
+    }
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            //?? is nil coalescing onImage insce offImage is an optional
+            return offImage ?? onImage
+        } else {
+            return onImage
+        }
+    }
+}
+
+#Preview {
+    RatingView(rating: .constant(4))
+}
