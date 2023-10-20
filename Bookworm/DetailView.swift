8ece50b150489e003db065b70bbec277c61fbdcf
micolCODES <micolromano124@gmail.com>
2023-10-20 18:02:51 +0200

    add alert before deleting in Detail view

--- Bookworm/DetailView.swift
+++ Bookworm/DetailView.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 struct DetailView: View {
     let book: Book
+    
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    @State private var showingDeleteAlert = false
 
     var body: some View {
         ScrollView {
@@ -38,6 +42,26 @@ struct DetailView: View {
         }
         .navigationTitle(book.title ?? "Unknown Book")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Delete Book", isPresented: $showingDeleteAlert) {
+            Button("Delete", role: .destructive, action: deleteBook)
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Are you sure?")
+        }
+        .toolbar {
+            Button {
+                showingDeleteAlert = true
+            } label: {
+                Label("Delete this book", systemImage: "trash")
+            }
+        }
+    }
+    
+    func deleteBook() {
+        moc.delete(book)
+        // commented out while testing
+        //try? moc.save()
+        dismiss()
     }
 }
 
