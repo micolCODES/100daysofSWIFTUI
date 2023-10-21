67d8f80368c1e2321ff268eeaf55a3a4d2e16030
micolCODES <micolromano124@gmail.com>
2023-10-21 11:13:57 +0200

    initial commit

---
+++ CoreDataProject/CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Micol on 21.10.23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
