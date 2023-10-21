2438f44c8aaa23d8fa290fd8d01ad37eb75ce009
micolCODES <micolromano124@gmail.com>
2023-10-21 11:22:42 +0200

    create DataController and CoreData Model

---
+++ CoreDataProject/CoreDataProject/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  CoreDataProject
+//
+//  Created by Micol on 21.10.23.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject") // not loaded yet, it's just being prepared to load it
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
