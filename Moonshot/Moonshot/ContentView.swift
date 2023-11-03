738579e70d5d139f6af7c8de0685df80fce38a9e
micolCODES <micolromano124@gmail.com>
2023-11-03 20:16:28 +0100

    review

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -18,7 +18,7 @@ struct ContentView: View {
     
     
     var body: some View {
-        NavigationView {
+        NavigationStack {
             ScrollView {
                 LazyVGrid(columns: columns) {
                     ForEach(missions) { mission in
