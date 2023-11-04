f7f01b370d5909a32a05fd2ae9af8d8ee9fad76c
micolCODES <micolromano124@gmail.com>
2023-11-04 12:21:45 +0100

    Review

--- MultiplicationTables/MultiplicationTables/ContentView.swift
+++ MultiplicationTables/MultiplicationTables/ContentView.swift
@@ -40,7 +40,7 @@ struct ContentView: View {
     
     
     var body: some View {
-        NavigationView {
+        NavigationStack {
             VStack {
                 //setup View
                 VStack {
